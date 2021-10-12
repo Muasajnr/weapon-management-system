f050e206b87d0df3c5f6934ad7dc22fec9142fd3
rasyidcode <jamil@dwebsite.net>
2021-10-13 03:29:43 +0800

    update

--- iramadibatu/app/Views/layouts/dashboard/sidebar.php
+++ iramadibatu/app/Views/layouts/dashboard/sidebar.php
@@ -11,10 +11,13 @@
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-            <img src="<?=site_url('themes/AdminLTE/dist/img/user2-160x160.jpg')?>" class="img-circle elevation-2" alt="User Image">
+            <!-- <img src="" class="img-circle elevation-2" alt="User Image"> -->
+            <div style="width: 35px; height: 35px; background-color:#bfbfbf; border-radius: 100%; padding-top: .3rem;" class="image-circle text-center">
+                AA
+            </div>
         </div>
         <div class="info">
-            <a href="#" class="d-block">Alexander Pierce</a>
+            <a href="#" class="d-block">testuser18</a>
         </div>
     </div>
 
